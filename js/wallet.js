58cf0d5b949fa125bb6dacd4c8a540d3eae277f1
AndrewGalak6578 <andrew.galaktionov@inbox.ru>
2023-07-03 17:01:32 +0600

    added a bit of packages

---
+++ js/wallet.js
@@ -0,0 +1,14 @@
+import {ConnectButton} from '@suiet/wallet-kit';
+import {WalletProvider} from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
+
+const App = () => {
+  return (
+    <>
+      <body>
+        <ConnectButton/>
+      </body>
+      
+    </>
+  )
+};
